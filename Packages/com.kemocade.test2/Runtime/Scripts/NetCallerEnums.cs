9ddebb9545b48d6e35f0d2427cccf7ff5c58a086
Dustuu <dustuu@furcade.com>
2023-06-06 12:02:08 +0800

    add imported code as package

---
+++ Packages/com.kemocade.test2/Runtime/Scripts/NetCallerEnums.cs
@@ -0,0 +1,44 @@
+﻿// Original Code by Miner28
+namespace Kemocade.Vrc.Net
+{
+    public enum SyncTarget
+    {
+        All,
+        Local,
+        Others
+    }
+
+    public enum Types
+    {
+        Byte,
+        SByte,
+        Int32,
+        UInt32,
+        Int64,
+        UInt64,
+        Int16,
+        UInt16,
+        Single,
+        Double,
+        String,
+        Boolean,
+        VRCPlayerApi,
+        Color,
+        Color32,
+        Vector3,
+        Quaternion,
+        ByteA,
+        SByteA,
+        Int32A,
+        UInt32A,
+        Int64A,
+        UInt64A,
+        Int16A,
+        UInt16A,
+        SingleA,
+        DoubleA,
+        StringA,
+        BooleanA,
+        VRCPlayerApiA
+    }
+}
